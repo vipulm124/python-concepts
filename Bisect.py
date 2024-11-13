7d8b16edd7dc9fa38f5904af004b3db9803783ab
Vipul Malhotra <vipulm124@gmail.com>
2024-11-14 02:36:04 +0530

    Added insort as well

--- Bisect.py
+++ Bisect.py
@@ -1,5 +1,6 @@
 
-from bisect import bisect, bisect_left, bisect_right
+# this just provided the index, where the no. should be added in sorted array, it does not add the value
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
 
 def bisect_it(nums, num):
     print(bisect(nums, num))
@@ -16,3 +17,21 @@ bisect_it(nums, 5)
 bisect_lft(nums, 5)
 
 bisect_rght(nums, 5)
+
+
+# TO add the value, we use insort
+
+def display(nums):
+    for i in nums:
+        print(i)
+    print("=======")
+
+def insort_it(nums, num):
+    display(nums)
+    insort(nums, num,7 ,8)
+    display(nums)
+
+# similary insort_left and insert_right works
+
+
+insort_it(nums, 4)
